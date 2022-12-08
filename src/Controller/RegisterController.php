cb7ed2304bbed7868f4dc4189213b64b573c2c11
Gabistam <gabistam@gmail.com>
2022-12-08 05:49:00 +0100

    registerController

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -7,14 +7,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegisterController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData(); //Récupère toute la data du formulaire appartenant à user
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+        }
         
         return $this->render('register/index.html.twig',[
             'form' => $form->createView()            
